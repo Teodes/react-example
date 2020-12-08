dd2f562d0ac76bd79934e3f72d75613ce00563ab
Teodes <jgrachot@gmail.com>
2020-12-08 19:39:47 -0300

    cambio en texto

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>-- Testing Danto Games Web --</h1>
+        <h1>-- Danto Games Web --</h1>
         <a
           className="App-link"
           target="_blank"
