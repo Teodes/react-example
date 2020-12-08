89cb9078a0e097e00b440719fa3c1a2684a1919e
Teodes <jgrachot@gmail.com>
2020-12-08 18:34:02 -0300

    cambio en el link

--- src/App.js
+++ src/App.js
@@ -9,11 +9,10 @@ function App() {
         <h1>-- Testing Danto Games Web --</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Coming Soon
         </a>
       </header>
     </div>
